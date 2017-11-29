478e63b4ceb9885aab42d8ad2d1a2daadd8f6452
sandybekn <sandybekn@gmail.com>
2017-11-29 21:26:11 +0600

    [week 14: Final project]

--- DrawingAppProject_nurzhamal/SettingsVC.swift
+++ DrawingAppProject_nurzhamal/SettingsVC.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+protocol SettingsVCDelegate:class {
+    func settingsViewCotrollerDidFinish(_ settingsVC:SettingsVC)
+}
 class SettingsVC: UIViewController {
 
     
@@ -26,10 +29,25 @@ class SettingsVC: UIViewController {
     var green:CGFloat = 0.0
     var blue:CGFloat = 0.0
     
+    var brushSize:CGFloat = 5.0
+    var opacityValue:CGFloat = 1.0
+    
+    var delegate: SettingsVCDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        drawPreview(red: red, green: green, blue: blue)
+        redSlider.value = Float(red)
+        redLabel.text = String(Int(redSlider.value * 255))
+        
+        greenSlider.value = Float(green)
+        greenLabel.text = String(Int(greenSlider.value * 255))
+        
+        blueSlider.value = Float(blue)
+        blueLabel.text = String(Int(blueSlider.value * 255))
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,33 +56,57 @@ class SettingsVC: UIViewController {
     }
     
     @IBAction func dismiss(_ sender: Any) {
+        if delegate != nil {
+            delegate?.settingsViewCotrollerDidFinish(self)
+        }
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func brushSizeChanged(_ sender: Any) {
+        let slider = sender as! UISlider
+        brushSize = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
+        
     }
     @IBAction func opacityCanged(_ sender: Any) {
+        let slider = sender as! UISlider
+        opacityValue = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
     }
     @IBAction func redSliderChanged(_ sender: Any) {
         let slider = sender as! UISlider
         red = CGFloat(slider.value)
         drawPreview(red: red, green: green, blue: blue)
-        redLabel.text = "\(slider.value = 255)"
+        redLabel.text = "\(Int(slider.value * 255))"
     }
     @IBAction func greenSliderChanged(_ sender: Any) {
         let slider = sender as! UISlider
         green = CGFloat(slider.value)
         drawPreview(red: red, green: green, blue: blue)
-        greenLabel.text = "\(slider.value = 255)"
+        greenLabel.text = "\(Int(slider.value * 255))"
     }
     @IBAction func blueSliderChanged(_ sender: Any) {
         let slider = sender as! UISlider
         blue = CGFloat(slider.value)
         drawPreview(red: red, green: green, blue: blue)
-        blueLabel.text = "\(slider.value = 255)"
+        blueLabel.text = "\(Int(slider.value * 255))"
     }
     
     func drawPreview(red: CGFloat, green: CGFloat, blue: CGFloat ){
-        imageView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        //imageView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacityValue).cgColor)
+        context?.setLineWidth(brushSize)
+        context?.setLineCap(CGLineCap.round)
+        
+        context?.move(to: CGPoint(x:70, y:70))
+        context?.addLine(to: CGPoint(x:70, y:70))
+        context?.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
     /*
     // MARK: - Navigation
--- DrawingAppProject_nurzhamal/ViewController.swift
+++ DrawingAppProject_nurzhamal/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Social
+
 
 class ViewController: UIViewController {
 
@@ -18,6 +20,9 @@ class ViewController: UIViewController {
     var red:CGFloat = 0.0
     var green:CGFloat = 0.0
     var blue:CGFloat = 0.0
+    
+    var brushSize:CGFloat = 5.0
+    var opacityValue:CGFloat = 1.0
     
     var isDrawing = true
     var selectedImage: UIImage!
@@ -46,8 +51,8 @@ class ViewController: UIViewController {
         
         context?.setBlendMode(CGBlendMode.normal)
         context?.setLineCap(CGLineCap.round)
-        context?.setLineWidth(5)
-        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: 1.0).cgColor)
+        context?.setLineWidth(brushSize)
+        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacityValue).cgColor)
         
         context?.strokePath()
         
@@ -83,6 +88,7 @@ class ViewController: UIViewController {
             (red,green,blue) = (0.41,0.06,0.5)
         }
     }
+    
     @IBAction func reset(_ sender: Any) {
         self.imageView.image = nil
     }
@@ -108,6 +114,22 @@ class ViewController: UIViewController {
         present(actionSheet, animated: true, completion: nil)
     }
     
+    @IBAction func tweetButton(_ sender: Any) {
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            
+            let tweetShare:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            self.present(tweetShare, animated: true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Accounts", message: "Please login to a Twitter account to tweet.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     @IBAction func erase(_ sender: Any) {
         if(isDrawing) {
             (red, green, blue) = (1, 1, 1)
@@ -124,10 +146,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        let settingsVC = segue.destination as! SettingsVC
+        settingsVC.delegate = self
+        settingsVC.red = red
+        settingsVC.green = green
+        settingsVC.blue = blue
+        settingsVC.brushSize = brushSize
+        settingsVC.opacityValue = opacityValue
+    }
 
 }
 
-extension ViewController:UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+extension ViewController:UINavigationControllerDelegate, UIImagePickerControllerDelegate, SettingsVCDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let imagePicked = info[UIImagePickerControllerOriginalImage] as? UIImage {
             self.selectedImage = imagePicked
@@ -138,5 +172,12 @@ extension ViewController:UINavigationControllerDelegate, UIImagePickerController
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    func settingsViewCotrollerDidFinish(_ settingsVC: SettingsVC) {
+        self.red = settingsVC.red
+        self.blue = settingsVC.blue
+        self.green = settingsVC.green
+        self.brushSize = settingsVC.brushSize
+        self.opacityValue = settingsVC.opacityValue
     }
 }
